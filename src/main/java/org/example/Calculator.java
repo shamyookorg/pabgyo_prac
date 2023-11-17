68bc5bc3601eaeed55ebebe7f244cebb202f494d
JulienJang <big00124@naver.com>
2023-11-17 14:18:14 +0900

    substract first commit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,4 +5,7 @@ public class Calculator {
     public int sumTwoNumbers(int num1, int num2) {
         return num1 + num2;
     }
+    public int substractTwoNumbers(int num1, int num2) {
+        return num1 - num2;
+    }
 }
