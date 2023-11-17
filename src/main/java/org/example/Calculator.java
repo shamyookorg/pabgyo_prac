99d19687ca10c6c584374c15fcaa25ac323778da
JulienJang <big00124@naver.com>
2023-11-17 13:54:15 +0900

    first commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculator {
+
+    public int sumTwoNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.sumTwoNumbers(1, 2));
+    }
+}
